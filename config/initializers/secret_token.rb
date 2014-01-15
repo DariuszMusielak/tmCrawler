ca7c0a58e9af77037028ddd5afb391797605d8d9
DariuszMusielak <dariusz.roman.musielak@gmail.com>
2014-01-15 12:04:56 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TmCrawler::Application.config.secret_key_base = 'e49a5e4f00286fbd4d51d58abecab0cd580b9b0b86df1b8bc86f9b9edb2e5d9ecd27956b4aa45d59de75c85e944e819d80434ec33e3869140cc56e889b690d81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TmCrawler::Application.config.session_store :cookie_store, key: '_tmCrawler_session'
